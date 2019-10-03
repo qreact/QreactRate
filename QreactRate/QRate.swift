6ca1c9f4f7cce5f3d408e0af546e986e56f3c6d5
hbayramov <hsyn.bayramov@gmail.com>
2019-10-03 10:12:29 +0400

    Added test cases

---
+++ QreactRate/QRate.swift
@@ -0,0 +1,22 @@
+//
+//  QRate.swift
+//  QreactRate
+//
+//  Created by Huseyn I. Bayramov on 10/3/19.
+//  Copyright © 2019 Huseyn I. Bayramov. All rights reserved.
+//
+
+import UIKit
+
+public final class QRate {
+    
+    let name = "QRate"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func multiply(a: Int, b: Int) -> Int {
+        return a * b
+    }
+}
--- QreactRateTests/QreactRateTests.swift
+++ QreactRateTests/QreactRateTests.swift
@@ -11,12 +11,23 @@ import XCTest
 
 class QreactRateTests: XCTestCase {
 
+    var rate: QRate!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        rate = QRate()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(rate.add(a: 1, b: 1), 2)
+    }
+    
+    func testMultiply() {
+        XCTAssertEqual(rate.multiply(a: 1, b: 2), 2)
     }
 
     func testExample() {
