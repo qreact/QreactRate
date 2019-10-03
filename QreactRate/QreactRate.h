e5d62f08d3da18ee2a548a28a750350bd9813ad1
hbayramov <hsyn.bayramov@gmail.com>
2019-10-03 09:47:26 +0400

    initial setup

---
+++ QreactRate/QreactRate.h
@@ -0,0 +1,19 @@
+//
+//  QreactRate.h
+//  QreactRate
+//
+//  Created by Huseyn I. Bayramov on 10/3/19.
+//  Copyright © 2019 Huseyn I. Bayramov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QreactRate.
+FOUNDATION_EXPORT double QreactRateVersionNumber;
+
+//! Project version string for QreactRate.
+FOUNDATION_EXPORT const unsigned char QreactRateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QreactRate/PublicHeader.h>
+
+
---
+++ QreactRateExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QreactRateExamples
+//
+//  Created by Huseyn I. Bayramov on 10/3/19.
+//  Copyright © 2019 Huseyn I. Bayramov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
